78128f7e11b52669d2ca9793f95c1b95b5a10600
Mallory Guerdet-Schuster <guerschu@colostate.edu>
2026-04-20 15:07:44 -0600

    Adding files for characters

---
+++ Characters.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+
+class Characters{
+
+    ArrayList<Object> inventory;
+    
+    int water_intake;
+
+    int food_intake;
+
+    int hungry;
+
+    int thirt;
+
+    ArrayList<Object> clothing;
+
+
+
+    
+
+}
