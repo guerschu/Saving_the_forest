e362599e2df2a99a791b918db06604d989ec0325
Mallory Guerdet-Schuster <guerschu@colostate.edu>
2026-04-20 15:30:39 -0600

    adding basics

--- Characters.java
+++ Characters.java
@@ -11,12 +11,27 @@ class Characters{
 
     int hungry;
 
-    int thirt;
+    int thirsty;
 
     ArrayList<Object> clothing;
 
 
+    public character(ArrayList inventory, int hungry, int thirsty, ArrayList clothing){
+        this.inventory = new ArrayList();
+        this.hungry = 100;
+        this.thirsty = 100;
+        this.clothing = new ArrayList();
+    }
 
+    public int is_hungry(){
+
+    }
+
+    public int is_thirsty(){
+        
+    }
+
+    
     
 
 }
